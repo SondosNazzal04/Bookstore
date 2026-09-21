b8f946d7338932b8f48aade8b431fe91e7df1a01
SondosNazzal04 <sondos.nazzal@outlook.com>
2026-09-22 00:27:21 +0300

    q5

---
+++ q5.js
@@ -0,0 +1,6 @@
+let num1 = prompt('enter the first number');
+let num2 = prompt('enter the second number');
+
+alert(num1 + num2);
+
+// the encountered problem was that num1 and num2 were not treated as numbers but as strings
