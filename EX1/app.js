991f7e7177abef4991a2aa1d3c4b93a92edae788
SondosNazzal04 <sondos.nazzal@outlook.com>
2026-09-22 22:59:29 +0300

    refactoring the code to ex1 to simpler functions and looping until giving the right answer

--- EX1/app.js
+++ EX1/app.js
@@ -1,12 +1,37 @@
+function checkMembership(membership)
+{
+	return membership != 'student' && membership != 'regular';
+}
+
+function welcomeUser(username, membership)
+{
+	if (membership == 'student')
+		alert('Welcome Scholar ' + username);
+	else
+		alert('Welcome Member ' + username);
+}
+
+function printInformation(informationArray)
+{
+	for (let i = 0; i < informationArray.length; i++)
+		console.log(informationArray[i]);
+}
+
 let username = prompt("Please enter your name:");
 let membership = prompt("Please eneter your membership type:");
 membership = membership.toLowerCase();
 
-alert(membership === 'student'? 'Welcome Scholar ' + username :
-	membership === 'regular'? 'Welcome Member ' + username:
-	'Welcome ' + username);
+while (checkMembership(membership))
+{
+	membership = prompt('please enter your membership type:\n(student, regular)');
+}
+
+welcomeUser(username, membership);
 
 let prefrence = prompt("Do you prefer fiction or non-fiction book genre");
 let borrowedBook = prompt("Please enter the title of the book that you want to borrow:");
 alert(borrowedBook + ' book is being reserved!');
-console.log(username + ' borrowed ' + borrowedBook);
+
+let informationArray = [username, membership, prefrence, borrowedBook];
+
+printInformation(informationArray);
